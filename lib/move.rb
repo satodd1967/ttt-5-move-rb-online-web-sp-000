a0ccee5a7eae92d2067b825386c07e4715b3bc73
Seth Todd <seth.todd@outlook.com>
2019-11-24 19:50:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,8 @@ end
 def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
-def move(board, converted_input, charachter = "X")
-  baord[converted_input] = "X"
+def move(board, converted_input, char = "X")
+  baord[converted_input] = char
 end
 
   
