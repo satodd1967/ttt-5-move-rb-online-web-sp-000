78690caa58aa68be3f3694dcb5511c2bc8b01309
Seth Todd <seth.todd@outlook.com>
2019-11-24 19:07:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,9 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+user_input = 1
 def input_to_index(user_input)
-  puts ().to_i - 1
+  puts user_input.to_i + 1
 end
 
   
